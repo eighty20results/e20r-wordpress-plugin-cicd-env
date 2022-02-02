dfc31ff2af4f0b9b6794529ac9bb6606b68099c0
Thomas Sjolshagen <thomas@eighty20results.com>
2022-02-02 13:10:54 +0100

    BUG FIX: Didn't include the 'ActivateUtilitiesPlugin' stuff.

--- e20r-wordpress-plugin-cicd-env.php
+++ e20r-wordpress-plugin-cicd-env.php
@@ -19,6 +19,8 @@
  *  You can contact us at mailto:info@eighty20results.com
  */
 
+namespace E20R\CICD_Environment;
+
 /*
 Plugin Name: E20R WordPress Plugin CICD Environment
 Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
@@ -28,3 +30,29 @@ Author: eighty20results
 Author URI: http://URI_Of_The_Plugin_Author
 License: A "Slug" license name e.g. GPL2
 */
+
+use E20R\Utilities\ActivateUtilitiesPlugin;
+
+if ( ! class_exists( 'E20R\CICD_Environment' ) ) {
+	// FIXME: Add class definition here for the plugin
+}
+/**
+ * Load the required E20R Utilities Module functionality
+ */
+require_once __DIR__ . '/ActivateUtilitiesPlugin.php';
+
+if ( function_exists( 'apply_filters' ) && ! apply_filters( 'e20r_utilities_module_installed', false ) ) {
+
+	$required_plugin = 'E20R WordPress Plugin CICD Environment';
+
+	if ( false === ActivateUtilitiesPlugin::attempt_activation() ) {
+		add_action(
+			'admin_notices',
+			function () use ( $required_plugin ) {
+				\E20R\Utilities\ActivateUtilitiesPlugin::plugin_not_installed( $required_plugin );
+			}
+		);
+
+		return false;
+	}
+}
